9dec139b41fe93fa6800f8a833f98a067988e5ad
Matej Holubec <matej.holubec@gmail.com>
2021-05-21 13:31:06 +0200

    feat: add messages

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,20 +1,22 @@
 <template>
   <CFlex direction="column" justify-content="space-between">
-    <div v-for="(message, i) in messages" :key="i" mb="5" v-chakra>
-      <b> {{ message.username }} </b>
-      {{
-        new Date(message.time).toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-        })
-      }}
-      <br />
-      {{ message.text }}
+    <div>
+      <div v-for="(message, i) in messages" :key="i" mb="5" v-chakra>
+        <b> {{ message.username }} </b>
+        {{
+          new Date(message.time).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })
+        }}
+        <br />
+        {{ message.text }}
+      </div>
     </div>
 
     <CFlex position="relative" w="100%" bottom="20px" gap="0.5rem">
-      <CInput v-model="current" placeholder="Enter"/>
-      <CButton  @click="send" >
+      <CInput v-model="current" placeholder="Enter" />
+      <CButton @click="send">
         Send
       </CButton>
     </CFlex>
@@ -24,7 +26,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-import store from '../store'
+import store from "../store";
 
 @Component
 export default class Chat extends Vue {
@@ -33,29 +35,29 @@ export default class Chat extends Vue {
       username: "Interviewer",
       text: "Welcome!",
       time: Date.now(),
-      userId: "999"
-    }
+      userId: "999",
+    },
   ];
-  
-  public current = ""
+
+  public current = "";
 
   mounted(): void {
-    this.$socket.on("chat_message", message => {
-      this.messages.push(message)
-    })
-  
+    this.$socket.on("chat_message", (message) => {
+      this.messages.push(message);
+    });
   }
 
   send(): void {
-    this.$socket.emit("chat_message", this.current)
+    this.$socket.emit("chat_message", this.current);
+    this.current = "";
   }
 }
 
 interface Message {
-    username: string;
-    text: string;
-    time: number;
-    userId: string;
+  username: string;
+  text: string;
+  time: number;
+  userId: string;
 }
 </script>
 
